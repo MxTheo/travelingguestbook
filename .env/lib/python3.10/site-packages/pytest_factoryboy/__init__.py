26dc484a23a1af1e44dc6f17e243fbe8d40de5ff
MxTheo <theoschutte@me.com>
2024-06-16 17:45:03 +0200

    Changed virtual environment name to .venv, so that I can have a file with .env where I can store the secret key. This I can get with dotenv. After waiting a while and restarting computer, the module suddenly got imported, where I had MissingImportError before

--- .env/lib/python3.10/site-packages/pytest_factoryboy/__init__.py
+++
@@ -1,4 +0,0 @@
-"""pytest-factoryboy public API."""
-from .fixture import LazyFixture, named_model, register
-
-__all__ = ("register", "named_model", "LazyFixture")
--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +24,10 @@ LOGOUT_REDIRECT_URL = "home"
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sb0*2p&s)&^b(m9xg8v)b%ysv8!aqu6412migr(izf1^*q46s='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') != 'False'
 
 ALLOWED_HOSTS = []
 
