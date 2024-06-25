6b8c8dd18ea1ac748628f997b579c030a36fa047
MxTheo <theoschutte@me.com>
2024-06-25 09:23:46 +0200

    removed travelingguestbook in allowed hosts, so that it is more generic and may solve some issues with deploying

--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') != 'False'
 
-ALLOWED_HOSTS = ['travelingguestbook.pythonanywhere.com']
-CSRF_TRUSTED_ORIGINS = ['https://travelingguestbook.pythonanywhere.com']
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
 
 
 # Application definition
