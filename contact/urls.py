bfbaa3900a9a2a7897cbd5eb08011f7c21435941
MxTheo <theoschutte@me.com>
2024-07-25 09:36:04 +0200

    Removed the code for implementing mail. This is code that is not used, but maybe will be used in the future. No code for future use should be in the codebase. YAGNI

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
-    path('success/', views.SuccessView.as_view(), name='success'),
     path('about/', views.about, name='about'),
     path('help/', views.help, name='help'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -33,35 +33,3 @@ def create_mailto_url():
     admin = User.objects.get(username='admin')
     mailto_url = f'mailto:{admin.email}'
     return mailto_url
-
-class SuccessView(TemplateView):
-    '''When smtp is implemented, user will be redirected to a success page after entering the contact form'''
-    template_name = 'contact/success.html'
-
-class ContactView(FormView):
-
-    form_class = ContactForm
-    template_name = 'contact/contact.html'
-
-    def get_success_url(self) -> str:
-        return reverse('success')
-
-    def form_valid(self, form):
-        email = form.cleaned_data.get('email')
-        subject = form.cleaned_data.get('subject')
-        message = form.cleaned_data.get('message')
-
-        full_message = f"""
-            Received message below from {email}, {subject}
-            __________________________
-
-
-            {message}
-"""
-        send_mail(
-            subject="Received contact from submission",
-            message=full_message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[settings.NOTIFY_EMAIL],
-        )
-        return super(ContactView, self).form_valid(form)
