9492d9ef7e36dcf1dbfeffa334e4496202ec3a9c
MxTheo <theoschutte@me.com>
2024-08-03 08:42:57 +0200

    Was not able to refactor for login users in unittests. Therefore, needed to remove the code I forgot to remove. As I do not use it anywhere

--- travelingguestbook/helpers_test.py
+++ travelingguestbook/helpers_test.py
@@ -1,15 +1,6 @@
 from django.urls import reverse
 
-from travelingguestbook.factories import UserFactory
-
 def helper_test_page_rendering(client, name_of_page, arguments=None):
     url = reverse(name_of_page, args=arguments)
     response = client.get(url)
     assert response.status_code == 200
-
-def login_user(client):
-    '''Custom login fixtur according to https://djangostars.com/blog/django-pytest-testing/,
-    to log in for test'''
-    user = UserFactory()
-    client.login(username=user.username, password=user.password)
-    return client, user
