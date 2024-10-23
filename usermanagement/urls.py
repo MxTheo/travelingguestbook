b524567b6cca64a0c0040f8529fbbea492144f73
MxTheo <theoschutte@me.com>
2024-10-23 13:18:42 +0200

    Changed jouwcodes to mijncodes. Little bit strange to call it jouw

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -4,7 +4,7 @@ from usermanagement import views
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
     path('dashboard/gespreksketting/', views.dashboard_logmessage, name='dashboard_logmessage'),
-    path('dashboard/jouwcodes/', views.dashboard_sociable, name='dashboard_sociable'),
+    path('dashboard/mijncodes/', views.dashboard_sociable, name='dashboard_sociable'),
     path('profile/<str:username>', views.UserDetail.as_view(), name='profile'),
     path('editaccount/', views.update_profile, name='editaccount'),
 ]
