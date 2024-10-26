7deaebee4ed8d85ee84887bbeeeaa2ae1d785cd7
MxTheo <theoschutte@me.com>
2024-10-26 09:29:01 +0200

    Main dashboard is now mijncodes

--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOGIN_REDIRECT_URL  = "dashboard_logmessage"
+LOGIN_REDIRECT_URL  = "dashboard_sociable"
 LOGOUT_REDIRECT_URL = "home"
 
 # Quick-start development settings - unsuitable for production
