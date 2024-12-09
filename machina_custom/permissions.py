e935018ecc253aa6696982f5e9cb3fae0b7f9f31
MxTheo <theoschutte@me.com>
2024-12-09 09:43:57 +0100

    #9 The permissions for the anonymous users has to be set for every forum manually by the admin. The code for doing that has been removed

--- machina_custom/permissions.py
+++
@@ -1,7 +0,0 @@
-"""Permissions for the anonymous users"""
-
-PERMISSIONS = {
-    'can_read_forum': {'is_active': True},
-    'can_start_new_topics': {'is_active': False},
-    'can_reply_to_topics': {'is_active': False},
-}
