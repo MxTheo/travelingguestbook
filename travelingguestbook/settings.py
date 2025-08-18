5c5b2a756aab2bf59741bc9e443e4a99c29b8726
MxTheo <theoschutte@me.com>
2025-08-18 10:44:31 +0200

    #96 The url of the sociable detail can now be copied The url is now also on top of the sociable detail page
    #99 The buttons are now shown for anonymous users, but the anonymous user does not have the rights yet
    
    #97 The sociable detail page is now simplified, without profile pictures and just the chat

--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOGIN_REDIRECT_URL  = "dashboard_sociable"
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "chat"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
