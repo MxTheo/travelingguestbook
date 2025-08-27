287fe86eb2e42a6802d1815b43677922c0a4bf52
MxTheo <theoschutte@me.com>
2025-08-27 11:01:56 +0200

    Edited admin.py so that I can see the date_created

--- oogcontact/admin.py
+++ oogcontact/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Registration
 
-admin.site.register(Registration)
-
 class RegistrationAdmin(admin.ModelAdmin):
     '''Admin interface for the Registration model.'''
     list_display = ('number', 'name', 'email', 'date_created', 'hasCanceled')
     search_fields = ('name', 'email')
     list_filter = ('hasCanceled', 'date_created')
     ordering = ('-date_created',)
+    readonly_fields = ('date_created', 'number')
+
+admin.site.register(Registration, RegistrationAdmin)
