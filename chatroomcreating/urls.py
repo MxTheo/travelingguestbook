ffd6ea6eb46e9f86f07a4f0d95f1be4340ac28f3
MxTheo <theoschutte@me.com>
2025-08-27 10:49:23 +0200

    #103 Also changed the url, wich now has uitwisseling

--- chatroomcreating/urls.py
+++ chatroomcreating/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('nieuwechat/', views.create_chatroom, name='create-chatroom'),
     path('verwijderchat/<slug:slug>', views.ChatRoomDelete.as_view(), name='delete-chatroom'),
 
-    path('chat', views.home, name='chat'),
+    path('uitwisseling', views.home, name='chat'),
 
     path('nieuwbericht/<slug:slug>', views.ChatMessageCreate.as_view(), name='create-chatmessage'),
     path('verwijderbericht/<str:pk>', views.ChatMessageDelete.as_view(), name='delete-chatmessage'),
