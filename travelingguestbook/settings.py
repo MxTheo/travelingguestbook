5af1557333915a6bc54d6344bebd7d0f7186c222
MxTheo <theoschutte@me.com>
2025-11-02 10:07:58 +0100

    media fix. Need to change pythonanywhere

--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -27,7 +27,7 @@ LOGOUT_REDIRECT_URL = "home"
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') != 'False'
 
 ALLOWED_HOSTS = ['www.bedanktvoorhetcontact.nl', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://www.bedanktvoorhetcontact.nl']
