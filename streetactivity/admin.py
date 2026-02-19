441e756652dadb293ccad29be435602bbc9febe7
MxTheo <theoschutte@me.com>
2026-02-19 09:29:15 +0100

    Register of Experience

--- streetactivity/admin.py
+++ streetactivity/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StreetActivity, Moment
+from .models import StreetActivity, Moment, Experience
 
 admin.site.register(StreetActivity)
 admin.site.register(Moment)
+admin.site.register(Experience)
