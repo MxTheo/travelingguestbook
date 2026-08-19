bed8a0f2cca253537b880f1b4d5124b8ed9dbe2b
MxTheo <theoschutte@me.com>
2026-08-19 10:18:15 +0200

    #200 Removal of btn-sm

--- travelingguestbook/settings.py
+++ travelingguestbook/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
